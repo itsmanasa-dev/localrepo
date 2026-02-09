acebc71faac6f1aae5dc4c446a49e9d93ff0ec5f
Manasa <manasat433@gmail.com>
2026-02-09 22:48:33 +0530

    Learnt commands

--- first.js
+++ first.js
@@ -1,10 +1,1 @@
-const profile={
-    userName:"manasaa.t_",
-    isfollow:true,
-    posts:0,
-    followers:248,
-    following:223,
-    bio:null,
-};
-
-console.log(profile);
+//const profile=none;
