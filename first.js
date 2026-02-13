f479de47486c9fff30fa5538ee9126ef76ddf60e
Manasa <manasat433@gmail.com>
2026-02-13 07:43:46 +0530

    basic loop

--- first.js
+++ first.js
@@ -1,15 +1,4 @@
-let marks=prompt("Enter the marks");
-let grade;
-if (marks>=80 && marks<=100){
-    grade="A";
-} else if (marks>=70 && marks<=89){
-    grade="B"; }
-else if (marks>=60 && marks<=69) {
-    grade="C";
-}else if (marks>=50 && marks<=59) {
-    grade="D";;
-}else{
-    grade="F";
+for(let i=0;i<5;i++){
+    console.log("hello");
 }
-
-console.log("According to the score the grade is:",grade);
+console.log("Loop has ended");
