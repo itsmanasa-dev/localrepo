6e80430a939bec47635d19443ee5715ce2ba98e1
Manasa <manasat433@gmail.com>
2026-02-13 07:12:11 +0530

    switch

--- first.js
+++ first.js
@@ -1,2 +1,13 @@
-let age=4;
-age>18?console.log("Adult"):console.log("Not adult");
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
