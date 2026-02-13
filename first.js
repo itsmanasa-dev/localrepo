750a7c5e64fca18faa223d84a6c486733104ccba
Manasa <manasat433@gmail.com>
2026-02-13 07:31:27 +0530

    ex2

--- first.js
+++ first.js
@@ -1,6 +1,13 @@
-let num=prompt("Enter a number");
-if (num%5==0){
-    console.log(num,"Divisible by 5");
-} else{
-    console.log(num,"NOt divisible by 5");
+let marks=prompt("Enter the marks");
+if (marks>=80 && marks<=100){
+    console.log("Grade A");
+} else if (marks>=70 && marks<=89){
+    console.log(marks,"Grade B"); }
+else if (marks>=60 && marks<=69) {
+    console.log(marks,"Grade C");
+}else if (marks>=50 && marks<=59) {
+    console.log(marks,"Grade D");
+}else{
+    console.log(marks,"Grade F")
 }
+
