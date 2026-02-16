a24a97122b1967e23fb227b8f7250fc8cbb7ad3b
Manasa <manasat433@gmail.com>
2026-02-16 08:23:59 +0530

    strings

--- first.js
+++ first.js
@@ -1,7 +1,2 @@
-let gameNum=25;
-let userNum=prompt("Guess the number");
-while(userNum!=gameNum){
-    let userNum=prompt("you entered wrong number,enter again");
-
-}
-console.log("Congratulations you have guessed the Number");
+let str="Manasa";
+console.log(str[9]);
