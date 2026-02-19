483d4eb6dc2965de5286592d256f6d1b6c0306cb
Manasa <manasat433@gmail.com>
2026-02-19 08:32:08 +0530

    example ques

--- first.js
+++ first.js
@@ -1,4 +1,8 @@
-let arr=["a","S","h","d"];
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
+let arr=[87,97,44,37,76,60]
+let sum=0;
+for(let val of arr){
+    sum+=val;
 }
+let avg=sum/arr.length;
+console.log(`Sum of the marks=${sum}`);
+console.log(`Average marks of the class=${avg}`);
