41df2ab7e658b8c738d17d72a1150e22879bd64c
Manasa <manasat433@gmail.com>
2026-02-21 22:44:17 +0530

    functions

--- first.js
+++ first.js
@@ -1,11 +1,4 @@
-let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-
-companies.shift();
-console.log(companies);
-
-companies.splice(1,1,"Ola");
-console.log(companies);
-
-companies.push("Amazon");
-console.log(companies);
-
+function myfun(msg){
+    console.log(msg);
+}
+myfun("hi");
